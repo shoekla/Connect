f1ddb565a8998c2f2260b6b111855c190f8590be
Abir <abirshukla@gmail.com>
2016-12-08 01:15:06 -0500

    Added comments for con.py

--- Mac Folder/con.py
+++ Mac Folder/con.py
@@ -14,14 +14,14 @@ files = [f for f in os.listdir('.') if os.path.isfile(f)]
 sendFiles = []
 for f in files:
 	if str(f) != "con.py" and str(f) != ".DS_Store":
-		sendFiles.append(str(f))
+		sendFiles.append(str(f)) #does not get un-needed files
 db = firebase.database()
 db.child("GivenFiles").remove()
 db.child("GivenFiles").push(str(sendFiles))
 already = str(db.child("RetrievedFiles").get().val())
 arr = []
 try:
-	begin = already.find(', u"')+4
+	begin = already.find(', u"')+4#gets retrieved files
 	end = -4
 	arr = eval(already[begin:end])
 except:
